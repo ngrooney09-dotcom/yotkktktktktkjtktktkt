0b02a9b4d55b2d047689eea2b8de587669801488
Long Nguyen <qnguyen78@my.bcit.ca>
2026-03-27 15:51:09 -0700

    btuhhhhh

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+function isPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
+}
+
+module.exports = { isPalindrome };
---
+++ palindrome.test.js
@@ -0,0 +1,33 @@
+const { isPalindrome } = require("./palindrome");
+
+describe("Palindrome Checker Tests", () => {
+
+  test("simple palindrome", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  test("not a palindrome", () => {
+    expect(isPalindrome("hello")).toBe(false);
+  });
+
+  test("case insensitive", () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  test("ignores spaces", () => {
+    expect(isPalindrome("nurses run")).toBe(true);
+  });
+
+  test("ignores punctuation", () => {
+    expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+  });
+
+  test("numbers palindrome", () => {
+    expect(isPalindrome("12321")).toBe(true);
+  });
+
+  test("empty string", () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+
+});
