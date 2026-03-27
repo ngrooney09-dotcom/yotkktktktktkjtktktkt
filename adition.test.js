0171721c01bcbc4e50107e18164a84a745162704
Calvin Mwin-Era <45calvin45@gmail.com>
2026-03-27 14:37:57 -0700

    initial commit

---
+++ adition.test.js
@@ -0,0 +1,18 @@
+//operations should return sum of two numbers
+
+const {addition} = require("./addition")
+
+test("2+4=6", () => {
+    expect(addition(2,4)).toBe(6)
+})
+
+test.skip("23 + 60 = 83", () => {
+    expect(addition(23, 60)).toBe(83)
+})
+
+test.each([[3, 4, 7], [5, -10, -5], [0, 2, 2]])(
+  'a + b = expected',
+  (a, b, expected) => {
+    expect(addition(a, b)).toBe(expected);
+  }
+);
